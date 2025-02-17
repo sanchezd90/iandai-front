42979ae53b7a7fecc5d69c8072153e635c10c0c8
Daniel <daniel.rueda@ibisdev.tech>
2025-02-17 17:52:00 -0300

    Home content updated

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -2,6 +2,6 @@
 describe('Home Page', () => {
     it('should display the home page', () => {
       cy.visit('http://localhost:3000');
-      cy.contains('Read our docs');
+      cy.contains('This site is under construction');
     });
   });
